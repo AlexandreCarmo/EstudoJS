1b27cb2e9f04dae4edfdbabbb1e62ec7c744c3c7
Casagrande <alexandre.carmo@sou.ufmt.br>
2024-02-06 01:34:01 -0400

    Console.log e tipos de dados em JS

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -1,1 +1,18 @@
+// Strings
+console.log('Hello World!'); 
 console.log("Hello World!");
+console.log(`Hello World!`);
+console.log(`"Hello" 'World!'`);
+
+// Int
+console.log(35);
+
+// Float
+console.log(1.25);
+
+// Tipos de dados em JS
+console.log(typeof(1.24)); // Number
+console.log(typeof(112)); // Number
+
+console.log(typeof('Hello World!')); // String
+console.log(typeof(`Hello World!`)); // String
