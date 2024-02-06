ff48ef4fdd5411f5ee69e56bb7810f235ba4af6f
Casagrande <alexandre.carmo@sou.ufmt.br>
2024-02-06 02:13:29 -0400

    Exercicios da Aula 03. Add Aulas 04 e 05.

---
+++ Aula 03/Exercicios/script.js
@@ -0,0 +1,3 @@
+// Exercício) Fazer com que o console imprima a frase: "Meu nome é 'NOME'. Estou aprendendo JavaScript às (HORÁRIO em number) da manhã!"
+
+console.log('Meu nome é "Casagrande". Estou aprendendo JavaScript às', 1 , 'da manhã!');
---
+++ Aula 05/script.js
@@ -0,0 +1,4 @@
+console.log("Hello World!!");
+console.log("Este texto será exibido no console do navegador!");
+
+alert("Hello World!"); // A função 'alert' só funciona na página web e não é executável no NodeJS
