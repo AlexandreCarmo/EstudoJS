b13d4276d93e809cd9e057a0497e7a50cf0c8088
Casagrande <alexandre.carmo@sou.ufmt.br>
2024-02-06 02:48:56 -0400

    Aula 07 - Constantes com 'const'

---
+++ Aula 07/script.js
@@ -0,0 +1,10 @@
+// Constantes com 'const' //
+// Diferent de uma variável 'let', uma constante sempre deve ser inicializada e não pode ser modificada após inicializada.
+
+const nome = 'João';
+console.log(nome);
+
+// Operações entre constantes
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const conta = primeiroNumero * segundoNumero;
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Aula 03 - Aula sobre uso do 'console.log' e tipos de variáveis number e string.
 Aula 04 - Comentários em códigos JS.
 Aula 05 - HTML + JS - Console na página web e função 'alert'.
+Aula 06 - Variáveis 'let'.
+Aula 07 - Constantes 'const' - Operações entre constantes/variáveis.
