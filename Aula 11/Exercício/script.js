dacbb3a9a3df901d49d089a086af304cae58b58d
Casagrande <alexandre.carmo@sou.ufmt.br>
2024-02-16 01:37:26 -0400

    Add Template Strings no exercicio da aula 11

--- Aula 11/Exercício/script.js
+++ Aula 11/Exercício/script.js
@@ -1,4 +1,4 @@
 let num01 = prompt('Digite o primeiro número:');
 let num02 = prompt('Digite o segundo número:');
 
-console.log((parseInt(num01) + parseInt(num02)));
+alert(`O resultado da sua conta foi: ${(parseInt(num01) + parseInt(num02))}`);
